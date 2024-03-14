489bfc26f6e5ed34b5d4b26b158196c58f5dc125
Huy Hiep <huyhiep4520@gmail.com>
2024-03-14 11:15:24 +0700

    fix

--- components/layouts/default/Footer.tsx
+++ components/layouts/default/Footer.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import React from "react";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function Footer() {
   return (
@@ -150,6 +151,10 @@ export default function Footer() {
               Contact
             </h6>
             <p className="mb-4 flex items-center justify-center md:justify-start">
+              <AccountCircleIcon className="mr-1"/>
+              PHẠM HUY NGỌC
+            </p>
+            <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -159,7 +164,7 @@ export default function Footer() {
                 <path d="M11.47 3.84a.75.75 0 011.06 0l8.69 8.69a.75.75 0 101.06-1.06l-8.689-8.69a2.25 2.25 0 00-3.182 0l-8.69 8.69a.75.75 0 001.061 1.06l8.69-8.69z" />
                 <path d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
               </svg>
-              New York, NY 10012, US
+              Mỏ - Địa Chất
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -171,7 +176,7 @@ export default function Footer() {
                 <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                 <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
               </svg>
-              info@example.com
+              phngoc176@gmail.com
             </p>
             <p className="mb-4 flex items-center justify-center md:justify-start">
               <svg
@@ -186,7 +191,7 @@ export default function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-              + 01 234 567 88
+              + 0965551706
             </p>
             <p className="flex items-center justify-center md:justify-start">
               <svg
@@ -201,7 +206,7 @@ export default function Footer() {
                   clip-rule="evenodd"
                 />
               </svg>
-              + 01 234 567 89
+              DCCTKH64A
             </p>
           </div>
         </div>
